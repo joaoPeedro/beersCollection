31cdd5b77c69c7c4c5cfb7cc9722f6e57beb9bbf
joaoPeedro <joao.p.baltazar@gmail.com>
2023-12-19 09:18:14 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,29 +8,6 @@ const nextConfig = {
     BEERS_GATEWAY: process.env.BEERS_GATEWAY,
     COMMENTS_GATEWAY: process.env.COMMENTS_GATEWAY,
   },
-  async headers() {
-    return [
-      {
-        // Permitir solicitações do IP específico para a rota /api/comments
-        source: "/api/comments",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://93.176.86.249",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type",
-          },
-        ],
-      },
-      // Adicione outras configurações de cabeçalho conforme necessário
-    ];
-  },
 };
 
 module.exports = nextConfig;
